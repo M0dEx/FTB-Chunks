50d620cc0e0ec2922b604aede79b22bc7c9e43df
Jakub Kubík <jakub.kubik.it@protonmail.com>
2022-06-21 12:04:05 +0200

    feat: remove duplicated chunk loading

--- fabric/src/main/java/dev/ftb/mods/ftbchunks/fabric/FTBChunksExpectedImpl.java
+++ fabric/src/main/java/dev/ftb/mods/ftbchunks/fabric/FTBChunksExpectedImpl.java
@@ -1,19 +1,11 @@
 package dev.ftb.mods.ftbchunks.fabric;
 
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.TicketType;
-import net.minecraft.world.level.ChunkPos;
 
 import java.util.UUID;
 
 public class FTBChunksExpectedImpl {
     public static void addChunkToForceLoaded(ServerLevel level, String modId, UUID owner, int chunkX, int chunkY, boolean add) {
-        ChunkPos chunkPos = new ChunkPos(chunkX, chunkY);
         level.setChunkForced(chunkX, chunkY, add);
-        if (add) {
-            level.getChunkSource().addRegionTicket(TicketType.FORCED, chunkPos, 2, chunkPos);
-        } else {
-            level.getChunkSource().removeRegionTicket(TicketType.FORCED, chunkPos, 2, chunkPos);
-        }
     }
 }
